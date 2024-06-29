bbec9e47525f5ea4307cd8e19d2bb66f48f079a8
Alexpm27 <213523@ids.upchiapas.edu.mx>
2024-06-29 02:56:38 -0600

    [UPDATE] status endpoint test

--- src/test/java/com/example/apitest/HealthControllerTest.java
+++ src/test/java/com/example/apitest/HealthControllerTest.java
@@ -34,7 +34,7 @@ public class HealthControllerTest {
                 .andReturn();
 
         String content = result.getResponse().getContentAsString();
-        assertEquals("OK", content);
+        assertEquals("NO OK", content);
     }
 
 }
